e1dd72dede2174d9a6caa8e6f3bd20f0d0fceaff
Rizsky <rizskysky@gmail.com>
2026-01-21 22:47:12 +0700

    feat: add gen random string

--- internal/usecase/usecase.go
+++ internal/usecase/usecase.go
@@ -1,1 +1,16 @@
 package usecase
+
+import (
+	"math/rand"
+	"time"
+)
+
+func generateRandomString(n int) string {
+	var letterRunes = []rune("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890")
+	b := make([]rune, n)
+	rand.NewSource(time.Now().UnixNano())
+	for i := range b {
+		b[i] = letterRunes[rand.Intn(len(letterRunes))]
+	}
+	return string(b)
+}
